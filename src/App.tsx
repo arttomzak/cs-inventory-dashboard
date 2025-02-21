00315a1e47ad09ff4f5275d412088eb89dd471c0
Arthur <arttomzak@gmail.com>
2025-02-20 23:36:05 -0600

    post react-native blunder

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
+import steamButton from './assets/sits_01.png';
 
 function App() {
 
   return (
-    <h1>pucio</h1>
+    <>
+
+    <nav className="flex relative justify-center items-center h-16 bg-gray-900 text-white font-bold">
+      
+      <h1 className="absolute left-1/2 transform -translate-x-1/2">CS2 Inventory Dashboard</h1>
+      
+      <button className="hover:opacity-90 absolute right-5">
+        <img src={steamButton} />
+      </button>
+      
+    </nav>
+
+      <div>
+      </div>
+    </>
+
+
   )
 }
 
